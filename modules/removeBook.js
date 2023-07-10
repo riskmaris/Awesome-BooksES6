a98e6ecc5ca47e4d7c6c6d5e9c9ce91559e1a94e
riskmaris <mariskaps20@gmail.com>
2023-07-10 15:05:45 +0300

    Modify modules

--- modules/removeBook.js
+++ modules/removeBook.js
@@ -2,10 +2,9 @@
 //     bookDetails.splice(index, 1);
 //     saveBooksToStorage(bookDetails);
 //   };
-  
+
 //   const saveBooksToStorage = (bookDetails) => {
 //     localStorage.setItem('booksData', JSON.stringify(bookDetails));
 //   };
-  
+
 //   export default removeBook;
-  
