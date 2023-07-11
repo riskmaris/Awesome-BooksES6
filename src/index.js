8dda7c134250bb0bfeba949dee7311bd64114195
riskmaris <mariskaps20@gmail.com>
2023-07-11 13:31:32 +0300

    solve linter error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {AllBooks} from '../modules/allBooks.js';
+// import {AllBooks} from '../modules/allBooks.js';
+import AllBooks from '../modules/allBooks.js';
 import updateTime from '../modules/updateTime.js';
 
 const mybooks = new AllBooks();
